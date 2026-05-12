4fa5d0ce1d3f70495d9863fbc9610f40d44d4498
kris-985 <kris.bakalov98@gmail.com>
2026-05-13 01:35:44 +0300

    feat: update navbar to display user info and logout functionality

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +11,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  protected readonly authService = inject(AuthService);
   protected isMenuOpen = false;
+
+  private readonly router = inject(Router);
 
   protected toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
@@ -17,5 +22,11 @@ export class Navbar {
 
   protected closeMenu(): void {
     this.isMenuOpen = false;
+  }
+
+  protected logout(): void {
+    this.authService.logout();
+    this.closeMenu();
+    void this.router.navigate(['/home']);
   }
 }
